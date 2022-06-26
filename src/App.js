202b047b1e219c96f278bac1d05c85b468994ce4
shaikh_md.007 <shaikhmohammad520@gmail.com>
2022-06-26 16:28:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {users.length > 0 && (
+        <>
+          {users.map((user) => (
+            <div className="post" key={user.id}>
+              <div className="userid">
+                <label>User Xyz </label>
+                <br></br>
+                <label> Posted by #user on: 12 feb </label>
+              </div>
+              <div>
+                <img src={user.url} />
+                <div className="commentbx">
+                  <input type="text" placeholder="Add A Comment.."></input>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <button>Post</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
